384b982b268e1dcecc289ac5a85cebeee5c8858c
Miloš Živković <milos.zivkovic@tendermint.com>
2025-02-18 13:17:12 +0100

    Remove leftover code

--- internal/common/common.go
+++ internal/common/common.go
@@ -4,15 +4,7 @@ import "github.com/gnolang/gno/tm2/pkg/std"
 
 const Denomination = "ugnot"
 
-var (
-	DefaultGasFee = std.Coin{
-		Denom:  Denomination,
-		Amount: 5,
-	}
-
-	// TODO remove
-	InitialTxCost = std.Coin{
-		Denom:  Denomination,
-		Amount: 1000000, // 1 GNOT
-	}
-)
+var DefaultGasFee = std.Coin{
+	Denom:  Denomination,
+	Amount: 5,
+}
